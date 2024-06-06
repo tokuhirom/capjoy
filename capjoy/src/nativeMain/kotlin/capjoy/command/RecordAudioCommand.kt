8fe510ead1ce46f398f41ff2ac0397dcc0c3d75c
Tokuhiro Matsuno <tokuhirom@gmail.com>
2024-06-06 22:21:32 +0900

    save file

--- capjoy/src/nativeMain/kotlin/capjoy/command/RecordAudioCommand.kt
+++ capjoy/src/nativeMain/kotlin/capjoy/command/RecordAudioCommand.kt
@@ -2,6 +2,7 @@ package capjoy.command
 
 import capjoy.eprintln
 import com.github.ajalt.clikt.core.CliktCommand
+import kotlinx.cinterop.BetaInteropApi
 import kotlinx.cinterop.ExperimentalForeignApi
 import kotlinx.cinterop.convert
 import kotlinx.cinterop.memScoped
@@ -11,13 +12,13 @@ import platform.CoreMedia.CMBlockBufferGetDataLength
 import platform.CoreMedia.CMSampleBufferGetDataBuffer
 import platform.CoreMedia.CMSampleBufferIsValid
 import platform.CoreMedia.CMSampleBufferRef
+import platform.Foundation.NSData
 import platform.Foundation.NSFileHandle
-import platform.Foundation.NSMutableData
+import platform.Foundation.NSFileManager
 import platform.Foundation.NSRunLoop
-import platform.Foundation.NSURL
 import platform.Foundation.closeFile
 import platform.Foundation.create
-import platform.Foundation.fileHandleForWritingToURL
+import platform.Foundation.fileHandleForWritingAtPath
 import platform.Foundation.run
 import platform.ScreenCaptureKit.SCContentFilter
 import platform.ScreenCaptureKit.SCDisplay
@@ -27,10 +28,12 @@ import platform.ScreenCaptureKit.SCStreamConfiguration
 import platform.ScreenCaptureKit.SCStreamOutputProtocol
 import platform.ScreenCaptureKit.SCStreamOutputType
 import platform.darwin.NSObject
+import platform.posix.exit
 import platform.posix.sleep
 
 @OptIn(ExperimentalForeignApi::class)
 class RecordAudioCommand : CliktCommand() {
+    @BetaInteropApi
     override fun run() {
         memScoped {
             // Screencapturekitのセットアップ
@@ -53,15 +56,20 @@ class RecordAudioCommand : CliktCommand() {
 
                 val contentFilter = SCContentFilter(display, excludingWindows = emptyList<Any>())
                 val stream = SCStream(contentFilter, captureConfiguration, null)
-                val outputFileURL = NSURL.fileURLWithPath("/tmp/output.m4a")
-                val fileHandle = NSFileHandle.fileHandleForWritingToURL(outputFileURL, error = null)
+                val fileHandle = memScoped {
+                    val file = "/tmp/output.m4a"
+                    if (!NSFileManager.defaultManager.fileExistsAtPath(file)) {
+                        NSFileManager.defaultManager.createFileAtPath(file, null, null)
+                    }
+                    val fileHandle = NSFileHandle.fileHandleForWritingAtPath(file)
+                    if (fileHandle == null) {
+                        println("Failed to create file handle")
+                        exit(1)
+                    }
+                    fileHandle
+                }
 
                 val streamOutput = object : NSObject(), SCStreamOutputProtocol {
-                    //     @kotlinx.cinterop.ObjCMethod public open fun stream(
-                    //     stream: platform.ScreenCaptureKit.SCStream,
-                    //     didOutputSampleBuffer: platform.CoreMedia.CMSampleBufferRef? /* = kotlinx.cinterop.CPointer<cnames.structs.opaqueCMSampleBuffer>? */,
-                    //     ofType: platform.ScreenCaptureKit.SCStreamOutputType
-                    //     ): kotlin.Unit { /* compiled code */ }
                     override fun stream(
                         stream: SCStream,
                         didOutputSampleBuffer: CMSampleBufferRef?,
@@ -80,8 +88,13 @@ class RecordAudioCommand : CliktCommand() {
                             val dataLength = CMBlockBufferGetDataLength(blockBufferRef)
                             val data = ByteArray(dataLength.convert())
                             CMBlockBufferCopyDataBytes(blockBufferRef, 0.convert(), dataLength, data.refTo(0))
+                            val nsData = NSData.create(
+                                bytes = data.refTo(0).getPointer(this@memScoped),
+                                length = data.size.convert()
+                            )
+                            println("Writing $nsData to file $fileHandle")
                             fileHandle?.writeData(
-                                NSMutableData.create(data.refTo(0), data.size.convert()),
+                                nsData,
                                 null
                             )
                         }
@@ -101,24 +114,26 @@ class RecordAudioCommand : CliktCommand() {
                         return@startCaptureWithCompletionHandler
                     }
                     println("Capture started")
-                }
 
-                println("Recording... Press ENTER to stop.")
-//                readLine()
+                    println("Recording... Press ENTER to stop.")
+                    // Uncomment this line if you want to stop recording manually by pressing ENTER
+                    // readLine()
 
-                sleep(10u)
+                    sleep(10u)
+                    println("Sleeped...")
 
-                stream.stopCaptureWithCompletionHandler { error ->
-                    if (error != null) {
-                        println("Failed to stop capture: ${error.localizedDescription}")
-                    } else {
-                        println("Capture stopped")
+                    stream.stopCaptureWithCompletionHandler { error ->
+                        if (error != null) {
+                            println("Failed to stop capture: ${error.localizedDescription}")
+                        } else {
+                            println("Capture stopped")
+
+                            fileHandle?.closeFile()
+
+                            exit(0)
+                        }
                     }
                 }
-
-                println("Recording stopped.")
-
-                fileHandle?.closeFile()
             }
 
             // Run the main run loop to process the asynchronous callback
