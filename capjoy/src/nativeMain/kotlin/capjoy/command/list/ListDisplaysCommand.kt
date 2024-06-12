a4d965bcad1af4bd52e3cd30f139a6417901f0d5
Tokuhiro Matsuno <tokuhirom@gmail.com>
2024-06-12 19:04:11 +0900

    list-format should support table format.

--- capjoy/src/nativeMain/kotlin/capjoy/command/list/ListDisplaysCommand.kt
+++ capjoy/src/nativeMain/kotlin/capjoy/command/list/ListDisplaysCommand.kt
@@ -1,10 +1,13 @@
 package capjoy.command.list
 
+import capjoy.command.list.utils.showTable
 import capjoy.handleContent
-import capjoy.model.command.ListDisplayOutput
 import capjoy.model.entity.Display
 import capjoy.toModel
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.options.default
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.types.choice
 import kotlinx.cinterop.BetaInteropApi
 import kotlinx.cinterop.ExperimentalForeignApi
 import kotlinx.serialization.encodeToString
@@ -14,6 +17,7 @@ import platform.ScreenCaptureKit.SCDisplay
 class ListDisplaysCommand : CliktCommand(
     "List all displays",
 ) {
+    private val format by option().choice("json", "table").default("table")
     private val json =
         Json {
             prettyPrint = true
@@ -32,10 +36,28 @@ class ListDisplaysCommand : CliktCommand(
                         frame = it.frame.toModel(),
                         width = it.width.toInt(),
                         height = it.height.toInt(),
+                        // FIXME: got "<SCDisplay: 0x600003a0ac00>"... maybe kotlin native's bug.
                         description = it.description,
                     )
                 }
-            println(json.encodeToString(ListDisplayOutput(got)))
+            when (format) {
+                "json" -> println(json.encodeToString(got))
+                "table" -> {
+                    val headers = listOf(
+                        "Display ID",
+                        "Width",
+                        "Height",
+                    )
+                    val data = got.map {
+                        listOf(
+                            it.displayId,
+                            it.width.toString(),
+                            it.height.toString(),
+                        )
+                    }
+                    showTable(headers, data)
+                }
+            }
         }
     }
 }
