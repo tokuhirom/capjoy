9f0f725cc737b9a23de7c7033e3dd4b0f5c1c3e4
Tokuhiro Matsuno <tokuhirom@gmail.com>
2024-06-13 01:18:51 +0900

    refactor video related code

--- capjoy/src/nativeMain/kotlin/capjoy/recorder/Target.kt
+++ capjoy/src/nativeMain/kotlin/capjoy/recorder/Target.kt
@@ -1,23 +1,52 @@
 package capjoy.recorder
 
+import kotlinx.cinterop.ExperimentalForeignApi
+import platform.CoreGraphics.CGRectGetHeight
+import platform.CoreGraphics.CGRectGetWidth
+import platform.ScreenCaptureKit.SCContentFilter
 import platform.ScreenCaptureKit.SCDisplay
 import platform.ScreenCaptureKit.SCShareableContent
 import platform.ScreenCaptureKit.SCWindow
 import platform.posix.exit
 
-fun findTarget(displayId: UInt?, windowId: UInt?) {
+data class Target(
+    val contentFilter: SCContentFilter,
+    val width: ULong,
+    val height: ULong,
+)
+
+@OptIn(ExperimentalForeignApi::class)
+fun findTarget(displayId: UInt?, windowId: UInt?, targetCallback: (Target) -> Unit) {
     if (windowId != null) {
         findWindowByWindowId(windowId) { window ->
             println("Window ${window.windowID}")
+            val contentFilter = SCContentFilter(desktopIndependentWindow = window)
+            val width = CGRectGetWidth(window.frame()).toULong()
+            val height = CGRectGetHeight(window.frame()).toULong()
+            targetCallback(Target(contentFilter, width, height))
         }
     } else {
+        fun buildTarget(display: SCDisplay, apps: List<*>): Target {
+            println("Display ${display.displayID}")
+            // To capture the entire display with ScreenCaptureKit, use initWithDisplay:includingApplications:exceptingWindows:
+            // instead of initWithDisplay:excludingWindows: with an empty list, as the latter does not work correctly.
+            // This workaround involves listing all running applications.
+            // For more details, see: https://federicoterzi.com/blog/screencapturekit-failing-to-capture-the-entire-display/
+            val contentFilter = SCContentFilter(display, includingApplications = apps, emptyList<Any>())
+            val width = display.width.toULong()
+            val height = display.height.toULong()
+            return Target(contentFilter, width, height)
+        }
+
         if (displayId != null) {
             findDisplayByDisplayId(displayId.toLong()) { display, applications ->
                 println("Display ${display.displayID}: ${display.width}x${display.height} - ${display.description}")
+                targetCallback(buildTarget(display, applications))
             }
         } else {
             findDefaultDisplay { display, applications ->
                 println("Display ${display.displayID}: ${display.width}x${display.height} - ${display.description}")
+                targetCallback(buildTarget(display, applications))
             }
         }
     }
