b683deb8d7b225a4ed5ba1885ab82d5b0a0319b3
Tokuhiro Matsuno <tokuhirom@gmail.com>
2024-06-12 18:58:08 +0900

    add table format support for list-windows command

--- capjoy/src/nativeMain/kotlin/capjoy/command/list/ListWindowsCommand.kt
+++ capjoy/src/nativeMain/kotlin/capjoy/command/list/ListWindowsCommand.kt
@@ -1,10 +1,16 @@
 package capjoy.command.list
 
+import capjoy.command.list.utils.filterTableCols
+import capjoy.command.list.utils.showTable
 import capjoy.handleContent
-import capjoy.model.command.ListWindowsOutput
 import capjoy.model.entity.Window
 import capjoy.toModel
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.options.default
+import com.github.ajalt.clikt.parameters.options.help
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.types.boolean
+import com.github.ajalt.clikt.parameters.types.choice
 import kotlinx.cinterop.BetaInteropApi
 import kotlinx.cinterop.ExperimentalForeignApi
 import kotlinx.serialization.encodeToString
@@ -14,6 +20,12 @@ import platform.ScreenCaptureKit.SCWindow
 class ListWindowsCommand : CliktCommand(
     "List all windows",
 ) {
+    private val showInactive: Boolean by option()
+        .boolean()
+        .help("Show inactive windows")
+        .default(false)
+    private val format by option().choice("json", "table").default("table")
+    private val verbose by option().boolean().default(false)
     private val json =
         Json {
             prettyPrint = true
@@ -26,6 +38,8 @@ class ListWindowsCommand : CliktCommand(
             val got =
                 content.windows.map { window ->
                     window as SCWindow
+                }.filter {
+                    showInactive || it.active
                 }.map {
                     Window(
                         active = it.active,
@@ -37,7 +51,45 @@ class ListWindowsCommand : CliktCommand(
                         windowLayer = it.windowLayer,
                     )
                 }
-            println(json.encodeToString(ListWindowsOutput(got)))
+            when (format) {
+                "json" -> println(json.encodeToString(got))
+                "table" -> {
+                    val headers = listOf(
+                        "Window ID",
+                        "Active",
+                        "onScreen",
+                        "Process ID",
+                        "Bundle ID",
+                        "App Name",
+                        "Title"
+                    )
+                    val rows = got.map {
+                        listOf(
+                            it.windowID.toString(),
+                            it.active.toString(),
+                            it.onScreen.toString(),
+                            it.owningApplication?.processID.toString(),
+                            it.owningApplication?.bundleIdentifier.toString(),
+                            it.owningApplication?.applicationName.toString(),
+                            it.title ?: "",
+                        )
+                    }
+                    if (verbose) {
+                        showTable(headers, rows)
+                    } else {
+                        val (filteredHeaders, filteredRows) = filterTableCols(
+                            listOf(
+                                "Window ID",
+                                "Bundle ID",
+                                "App Name",
+                                "Title"
+                            ),
+                            headers, rows
+                        )
+                        showTable(filteredHeaders, filteredRows)
+                    }
+                }
+            }
         }
     }
 }
---
+++ capjoy/src/nativeMain/kotlin/capjoy/command/list/utils/TableUtils.kt
@@ -0,0 +1,39 @@
+package capjoy.command.list.utils
+
+import kotlin.math.max
+
+fun showTable(headers: List<String>, rows: List<List<String>>) {
+    val colWidths = headers.mapIndexed { index, header ->
+        max(header.length, rows.maxOfOrNull { it[index].length } ?: 0)
+    }
+
+    fun padString(value: String, length: Int): String {
+        return value.padEnd(length, ' ')
+    }
+
+    val formattedHeaders = headers.mapIndexed { index, header ->
+        padString(header, colWidths[index])
+    }.joinToString(" | ")
+
+    println(formattedHeaders)
+    println(colWidths.joinToString("-|-") { "-".repeat(it) })
+
+    rows.forEach { row ->
+        val formattedRow = row.mapIndexed { index, column ->
+            padString(column, colWidths[index])
+        }.joinToString(" | ")
+        println(formattedRow)
+    }
+}
+
+
+fun filterTableCols(
+    filterHeaders: List<String>,
+    headers: List<String>,
+    rows: List<List<String>>
+): Pair<List<String>, List<List<String>>> {
+    val indices = filterHeaders.map { headers.indexOf(it) }.filter { it != -1 }
+    val filteredHeaders = indices.map { headers[it] }
+    val filteredRows = rows.map { row -> indices.map { row[it] } }
+    return Pair(filteredHeaders, filteredRows)
+}
